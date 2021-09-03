a90d21f3fb70fd12422ecbd70129befe75bfa176
br-wa <brwa@mit.edu>
2021-09-02 22:12:55 -0400

    improve readability

--- nmk.sh
+++ nmk.sh
@@ -4,13 +4,39 @@
 # 2 = do template.tex
 # 3 = clean 
 
+USAGE="""Usage:
+	1. nmk.sh str -> compiles main.tex to str.pdf, and then cleans
+	2. nmk.sh str MN -> compiles with -pvc template.tex (with 'dayXY' replaced by 'dayMN') to str.pdf 
+	3. nmk.sh -c str MN -> cleans up 2
+"""
+
+CLEANING=0
+
+while getopts "ch" option; do 
+	case $option in
+		c) 
+			CLEANING=1
+			;;
+		h) 
+			printf "$USAGE"
+			exit 0
+			;;
+		?)
+			printf "Invalid option -${OPTARG}.\n"
+			printf "$USAGE"
+			exit 1
+			;;
+	esac
+done
+
+shift $((OPTIND-1))
+
 ALL=1
 
 if [ $# -eq 2 ]; then
-   	if [ ${#2} -eq 2 ]; then
-		ALL=2
-		DAY="$2"
-	else
+	ALL=2
+	DAY="$2"
+	if [ $CLEANING -eq 1 ]; then
 		ALL=3
 	fi
 fi
